6a1ea97c0c3a7e8e0c4d3a7a5f2dec113ddb86ed
Inhyuk Park <exilee20c@gmail.com>
2018-10-12 11:27:06 +0900

    Updates

---
+++ precache-manifest.fafe27dac6abddd9e0cf2b147d880022.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "827ae377c2bad99af586",
+    "url": "/my-app/static/js/main.827ae377.chunk.js"
+  },
+  {
+    "revision": "60886ce7ece2e436aeea",
+    "url": "/my-app/static/js/1.60886ce7.chunk.js"
+  },
+  {
+    "revision": "827ae377c2bad99af586",
+    "url": "/my-app/static/css/main.27bd9c2d.chunk.css"
+  },
+  {
+    "revision": "8a199887cc511251cc3fdc48f160e5a2",
+    "url": "/my-app/index.html"
+  }
+];
