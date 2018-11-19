557cde63b2d713874971128201ac4a687847b3bb
Inhyuk Park <exilee20c@gmail.com>
2018-11-19 13:28:02 +0900

    Updates

---
+++ precache-manifest.737eab4a9127605ee0b4696aa5d4a2ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1307e904cd22cbafa9c9",
+    "url": "/static/js/main.1307e904.chunk.js"
+  },
+  {
+    "revision": "40a0486e7b22aae23bf7",
+    "url": "/static/js/1.40a0486e.chunk.js"
+  },
+  {
+    "revision": "1307e904cd22cbafa9c9",
+    "url": "/static/css/main.2eed4423.chunk.css"
+  },
+  {
+    "revision": "3e0f0113a8bc61b9b23c6f1b28b9c654",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.70da06fe466fef1735103584255e8476.js"
+  "/precache-manifest.737eab4a9127605ee0b4696aa5d4a2ab.js"
 );
 
 workbox.clientsClaim();
