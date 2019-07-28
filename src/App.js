b4a45c6c6fcbd6c04aaf0de3a5c5891a87f24096
Inhyuk Park <exilee20c@mycelebs.com>
2019-07-29 00:13:51 +0900

    MemoDocker 제거

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import styled from "styled-components";
 import HeaderContent from "./HeaderContent";
 import RoutingBody from "./RoutingBody";
 import Scrollable from "hide-scrollbar-react";
-import MemoDocker from "todo-docker-react";
 
 const BODY_WIDTH = "1024px";
 
@@ -70,7 +69,6 @@ class ExileeWebApp extends Component {
 
             <RoutingBody maxWidth={BODY_WIDTH} />
           </Scrollable>
-          <MemoDocker title="Notepad" />
         </Fragment>
       </Router>
     );
